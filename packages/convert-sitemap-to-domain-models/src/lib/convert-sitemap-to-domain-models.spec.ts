19169335487849ad9c57ea26f4884aa9fc4f55a0
Sergey Lukin <contact@sergeylukin.com>
2023-03-06 00:14:02 +0200

    Implement collections & pages extraction logic

--- packages/convert-sitemap-to-domain-models/src/lib/convert-sitemap-to-domain-models.spec.ts
+++ packages/convert-sitemap-to-domain-models/src/lib/convert-sitemap-to-domain-models.spec.ts
@@ -1,9 +1,19 @@
-import { convertSitemapToDomainModels } from './convert-sitemap-to-domain-models';
+import {
+  convertSitemapToDomainModels,
+  convertURIListToCollectionsNamesList,
+} from './convert-sitemap-to-domain-models';
 
-describe('convertSitemapToDomainModels', () => {
-  it('should work', () => {
-    expect(convertSitemapToDomainModels()).toEqual(
-      'convert-sitemap-to-domain-models'
-    );
+describe('convertURIListToCollectionsNamesList', () => {
+  it('extracts paths that have multiple nodes as collections and others as pages', () => {
+    expect(
+      convertURIListToCollectionsNamesList([
+        'https://example.com/foo/1',
+        'https://example.com/foo/2',
+        'https://example.com/bar/',
+      ])
+    ).toEqual({
+      collections: ['foo'],
+      pages: ['/bar'],
+    });
   });
 });
--- packages/convert-sitemap-to-domain-models/src/lib/convert-sitemap-to-domain-models.ts
+++ packages/convert-sitemap-to-domain-models/src/lib/convert-sitemap-to-domain-models.ts
@@ -1,3 +1,36 @@
 export function convertSitemapToDomainModels(): string {
   return 'convert-sitemap-to-domain-models';
 }
+
+export const convertURIListToCollectionsNamesList = (list: string[]) => {
+  const models = list.reduce<{
+    collectionInstancesMap: { [key: string]: number };
+    collectionsNames: string[];
+    pagesURIs: string[];
+  }>(
+    (prev, curr) => {
+      const parts = curr.split('/');
+      const category = parts[3];
+      const page = `/${category}`;
+      if (!prev.collectionInstancesMap?.[category]) {
+        prev.collectionInstancesMap[category] = 1;
+        if (page) prev.pagesURIs.push(page);
+      } else {
+        if (prev.collectionsNames.indexOf(category) === -1)
+          prev.collectionsNames.push(category);
+        const pageIndex = prev.pagesURIs.indexOf(page);
+        prev.pagesURIs.splice(pageIndex, 1);
+      }
+      return prev;
+    },
+    {
+      collectionInstancesMap: {},
+      collectionsNames: [],
+      pagesURIs: [],
+    }
+  );
+  return {
+    collections: models.collectionsNames,
+    pages: models.pagesURIs,
+  };
+};
